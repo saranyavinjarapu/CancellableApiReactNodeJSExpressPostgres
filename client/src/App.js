89ef1484e807d39b9226ba103652eef88c9c080c
saranya vinjarapu <68265309+saranyavinjarapu@users.noreply.github.com>
2023-02-20 13:01:46 +0530

    added backend functions to retreive data and display it on frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,37 @@
+import { useState } from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
-  return <div className="App">hello</div>;
+  const [users, setUsers] = useState([]);
+  const getUsers = () => {
+    axios
+      .get("users")
+      .then((res) => {
+        console.log("recieved res is :", res);
+        setUsers(res.data);
+      })
+      .catch(function (thrown) {
+        let errorResponseMessage = thrown.response.data;
+        console.log(
+          errorResponseMessage ? errorResponseMessage : thrown.message
+        );
+      });
+  };
+  return (
+    <div className="App">
+      <button onClick={getUsers}>GET USERS</button>
+      <div className="DataContainer">
+        {users && (
+          <ul>
+            {users.map((user) => (
+              <li key={user.id}>{user.name}</li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const db = require("./connection");
 const app = express();
-const port = 5001;
+const port = 5002;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -9,6 +10,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", (request, response) => {
   response.json({ message: "NodeJS Express Postgres API " });
 });
+
+app.get("/users", db.getUsers);
 
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
